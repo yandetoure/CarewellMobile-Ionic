b9538f3e7fa5e8d1ed27e66554ff2bbaf87d2f88
MACOS <macos@air-de-macos.home>
2024-09-21 11:11:13 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'CarewellMobile',
+  webDir: 'www'
+};
+
+export default config;
